78f072b676e2981c42572dff57bdbf81f1719155
Mythril <169342+mythril@users.noreply.github.com>
2023-07-08 23:56:59 -0600

    cleaned up onedigit display in anticipation of new animations

--- src/components/games/chosen-one/OneDigitDisplay.svelte
+++ src/components/games/chosen-one/OneDigitDisplay.svelte
@@ -1,46 +1,51 @@
 <script lang="ts">
   export let value: number | undefined = undefined;
-  let prev: string;
-  let current: string;
   let next: string;
+  let current: string;
+  let prev: string;
   $: {
     if (value === undefined) {
-      prev = ';';
-      current = '';
       next = '';
+      current = '';
+      prev = '';
     } else {
-      prev = ((value - 1) % 10).toFixed(0);
-      current = (value % 10).toFixed(0);
       next = ((value + 1) % 10).toFixed(0);
+      current = (value % 10).toFixed(0);
+      prev = ((value - 1) % 10).toFixed(0);
     }
   }
 </script>
 
 <div class="digit">
-  <div class="prev">
-    {prev}
-  </div>
-  <div class="current">
-    {current}
-  </div>
-  <div class="next">
-    {next}
+  <div class="animwrap">
+    <div class="next">
+      {next}
+    </div>
+    <div class="current">
+      {current}
+    </div>
+    <div class="prev">
+      {prev}
+    </div>
   </div>
 </div>
 
 <style lang="scss">
   .digit {
+    box-sizing:border-box;
     position: relative;
     font-size: px(23);
     font-weight: 400;
     line-height: px(30);
-    width: px(22);
+    width: px(30);
+    height:100%;
     border-top-left-radius: px(6) px(22);
     border-bottom-left-radius: px(6) px(22);
-    padding: px(2) px(7) px(5) px(6);
     background-color: hsl(0, 0%, 50%);
     color: hsl(0, 0%, 93%);
-    margin-left: px(-12);
+    padding-left: px(5);
+    margin-left: px(-8);
+    left:px(-3);
     font-family: label-font();
     text-shadow: text-outline(rgba(0, 0, 0, 0.8), true);
     box-shadow: inset 0 px(15) px(2) px(2) rgba(0, 0, 0, 0.5),
@@ -48,19 +53,15 @@
       px(-1) 0 px(0.5) 0 rgba(0, 0, 0, 0.25),
       inset 0 px(-11) px(2) 0 rgba(0, 0, 0, 0.8),
       inset 0 px(6) px(2) 0 rgba(0, 0, 0, 0.8);
+    .next,.prev,.current{
+      height:px(35);
+    }
   }
-  .prev,
-  .current,
-  .next {
+  .animwrap {
     position: absolute;
-  }
-  .current {
-    top: 0%;
-  }
-  .prev {
     top: -100%;
-  }
-  .next {
-    top: 100%;
+    display:flex;
+    flex-flow:column;
+    transition:top 0.2s cubic-bezier(0.68, -0.55, 0.265, 1.55);
   }
 </style>
