fa16c5910540c7d61d6ee099f590a0d13c450ff6
Mythril <169342+mythril@users.noreply.github.com>
2023-07-21 12:26:59 -0600

    Factored out "plate button" into a component

---
+++ src/routes/chosen-one/PlateButton.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Bolthead from './Bolthead.svelte';
+</script>
+
+<button class={$$props.class}>
+  <Bolthead
+    dir="tl"
+    size={4}
+  />
+  <Bolthead
+    dir="tr"
+    size={4}
+  />
+  <Bolthead
+    dir="br"
+    size={4}
+  />
+  <Bolthead
+    dir="bl"
+    size={4}
+  />
+  <div class="label worn-text">
+    <slot />
+  </div>
+</button>
+
+<style lang="scss">
+  button {
+    position: relative;
+    padding: px(0) px(10);
+    display: block;
+    appearance: none;
+    background-color: $root-bg;
+    border: 0;
+    &::after {
+      content: ' ';
+      position: absolute;
+      left: px(12);
+      top: px(5);
+      @include push-button(18);
+    }
+    &:active::after {
+      @include push-button-active(18);
+    }
+    .label {
+      font-size: px(22);
+      text-align: left;
+      padding-left: px(25);
+    }
+    border-top-right-radius: px(2);
+    border-bottom-right-radius: px(10) px(4);
+    border-top-left-radius: px(2);
+    border-bottom-left-radius: px(10) px(4);
+    box-shadow: px(-1) px(1) px(2) px(-1) rgba(#fff, 0.5) inset,
+      px(-1) px(-1) px(0) px(-3) rgba(#000, 0.5) inset;
+  }
+</style>
