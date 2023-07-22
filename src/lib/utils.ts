4e066ca0e54bc1f343f7873297c4349406f477f4
Mythril <169342+mythril@users.noreply.github.com>
2023-07-22 12:26:09 -0600

    Added: UI sounds to be used later

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
-import clickFile from '$lib/sounds/click.wav';
+import clickFile from '$lib/sounds/click2.wav';
+import bonkFile from '$lib/sounds/bonk.wav';
 
 export function blobToDataURL(blob: Blob): Promise<string> {
   return new Promise<string>((resolve, reject) => {
@@ -10,7 +11,30 @@ export function blobToDataURL(blob: Blob): Promise<string> {
   });
 }
 
+// preload, only one instance (module singleton)
+const preloads: HTMLAudioElement[] = [];
 const clickAudio = new Audio(clickFile);
+preloads.push(clickAudio);
+const bonkAudio = new Audio(bonkFile);
+preloads.push(bonkAudio);
+
+for (const preload of preloads) {
+  try {
+    // keeps the audio from being truncated
+    preload.preload = 'auto';
+    preload.volume = 0;
+    await preload.play();
+  } catch {
+    // this is an expected error when we don't care if sound is played or not
+  } finally {
+    preload.volume = 1;
+  }
+}
+
 export function clickSound() {
   clickAudio.play();
-};
+}
+
+export function bonkSound() {
+  clickAudio.play();
+}
