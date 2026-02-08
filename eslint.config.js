8c7123fd104cd0e2e336a04547ff7060aa6921ba
Mythril <169342+mythril@users.noreply.github.com>
2026-02-08 14:11:02 -0700

    Restored linter functionality

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import prettier from 'eslint-config-prettier';
+import path from 'node:path';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      'no-undef': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' }
+      ],
+      'no-warning-comments': [
+        1,
+        { terms: ['todo', 'fixme'], location: 'anywhere' }
+      ],
+      'no-console': ['error']
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig
+      }
+    }
+  },
+  globalIgnores([
+    '**/.DS_Store',
+    '**/node_modules',
+    'build',
+    'nwcp',
+    'ignored',
+    '.svelte-kit',
+    'package',
+    '**/.env',
+    '**/.env.*',
+    '!**/.env.example',
+    '**/pnpm-lock.yaml',
+    '**/package-lock.json',
+    '**/yarn.lock'
+  ])
+]);
